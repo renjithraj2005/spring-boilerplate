d8b3ae6def5b335e64c50813d530ac3c8a575996
Renjith Raj <renjithraj2005@icloud.com>
2021-05-21 10:41:52 +0530

    Thread config added for async

--- src/main/java/com/boilerplate/demo/ApplicationRunner.java
+++ src/main/java/com/boilerplate/demo/ApplicationRunner.java
@@ -9,9 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableAsync
+@EnableScheduling
 public class ApplicationRunner {
 	
 	@Autowired
---
+++ src/main/java/com/boilerplate/demo/config/ThreadConfig.java
@@ -0,0 +1,30 @@
+package com.boilerplate.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class ThreadConfig {
+
+    @Bean
+    @Primary
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Sayone-AsyncThread");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(ThreadPoolTaskExecutor delegate) {
+        return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
+    }
+}
